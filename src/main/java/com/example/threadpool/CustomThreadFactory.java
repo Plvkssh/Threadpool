2200ffc757f1e9e2e998df434f195799d5ec2912
Plvkssh <kksmartregion@gmail.com>
2025-06-10 19:05:19 +0700

    Update CustomThreadFactory.java

--- src/main/java/com/example/threadpool/CustomThreadFactory.java
+++ src/main/java/com/example/threadpool/CustomThreadFactory.java
@@ -5,41 +5,22 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class CustomThreadFactory implements ThreadFactory {
     private static final Logger logger = Logger.getLogger(CustomThreadFactory.class.getName());
     private final AtomicInteger counter = new AtomicInteger(0);
     private final String poolName;
 
-    /**
-     * Создает фабрику для пула с указанным именем.
-     * @param poolName Имя пула (для логирования и префикса потоков).
-     */
     public CustomThreadFactory(String poolName) {
         this.poolName = poolName;
     }
 
     @Override
     public Thread newThread(Runnable r) {
-        // Формируем уникальное имя потока
-        String threadName = poolName + "-worker-" + counter.incrementAndGet();
-        Thread thread = new Thread(r, threadName);
-
-        // Настраиваем обработчик неожиданных исключений для логирования завершения
+        Thread thread = new Thread(r, poolName + "-worker-" + counter.incrementAndGet());
         thread.setUncaughtExceptionHandler((t, e) -> 
-            logger.log(Level.SEVERE, String.format("[Worker] Thread %s terminated due to uncaught exception: %s", t.getName(), e))
+            logger.log(Level.SEVERE, "Thread " + t.getName() + " failed", e)
         );
-
-        // Логируем создание потока
-        logger.log(Level.INFO, "[ThreadFactory] Creating new thread: {0}", threadName);
-
+        logger.log(Level.INFO, "Created new thread: " + thread.getName());
         return thread;
-    }
-
-    /**
-     * @return Общее количество созданных потоков.
-     */
-    public int getThreadsCreated() {
-        return counter.get();
     }
 }
